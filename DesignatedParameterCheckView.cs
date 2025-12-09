fff860b43cc8b44e3e74b4f7413f36478532e381
krion6 <vinayakam@krionconsulting.com>
2025-12-09 16:52:37 +0530

    Removed
    Removed

--- DesignatedParameterCheckView.cs
+++ DesignatedParameterCheckView.cs
@@ -1,15 +1,1 @@
-using Autodesk.Revit.DB;
-using QC__Checker.ViewModel;
-using System.Windows;
 
-namespace QC__Checker.Views
-{
-    public partial class DesignatedParameterCheckView : Window
-    {
-        public DesignatedParameterCheckView(Document doc, ElementId viewId)
-        {
-            InitializeComponent();
-            DataContext = new DesignatedParameterCheckSummaryViewModel(doc, viewId);
-        }
-    }
-}
