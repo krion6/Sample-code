35b9ff688660d0c7bc7e7cbf3b736124be146fff
krion6 <vinukrion@gmail.com>
2025-12-30 11:54:37 +0530

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Login
+{
+    static void Main()
+    {
+        string password = "admin123"; // ❌ Hardcoded credential
+        Console.WriteLine("Login successful");
+    }
+}
